2740d6aef0e735cb6379ed9720a99467c6a90c0d
Mujtaba Jawadi <mujtabafalcon986@gmail.com>
2024-10-17 12:51:26 +0500

    Added Error Page for URL Route Errors

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,15 +1,16 @@
-import { NavLink, useRouteError } from "react-router-dom"
+import { NavLink, useRouteError } from "react-router-dom";
 
 export const ErrorPage = () => {
-  
-  const error = useRouteError()
-  console.log(error)
+  const error = useRouteError();
+  console.log(error);
 
   return (
     <>
       <h1>Oops! A {error.status} error occurred</h1>
-      <h3>{error && error.data }</h3>
-      <NavLink to="/"><button>Go to Home Page</button></NavLink>
+      <h3>{error && error.data}</h3>
+      <NavLink to="/">
+        <button>Go to Home Page</button>
+      </NavLink>
     </>
-  )
-}
+  );
+};

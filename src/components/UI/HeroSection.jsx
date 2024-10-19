de1c7a728d1b5e8dd8e4502173e763e59741e9c6
Mujtaba Jawadi <mujtabafalcon986@gmail.com>
2024-10-19 11:50:29 +0500

    Adjusting styling of Hero-Section, Completed About page by adding JSON file and styling of About Page.Making separate Components for HeroSection and adding it to the Home page along with About Page.

---
+++ src/components/UI/HeroSection.jsx
@@ -0,0 +1,33 @@
+import { FaLongArrowAltRight } from "react-icons/fa";
+
+export const HeroSection = () => {
+  return (
+    <main className="hero-section main">
+      <div className="container grid-col-two">
+        <div className="hero-content">
+          <h1 className="slogan-text">
+            Flags, Facts and Beyond-
+            <br />
+            Explore the Earth
+          </h1>
+          <p className="paragraph">
+            &quot;Embark on a journey through the nations of the world. Explore
+            their unique flags, delve into their rich histories and uncover the
+            landmarks that define their cultures. Every country has a story
+            waiting to be discovered.&quot;
+          </p>
+          <button>
+            Start Exploring <FaLongArrowAltRight />
+          </button>
+        </div>
+        <div className="hero-image">
+          <img
+            src="/images/globe.png"
+            alt="Globe Image"
+            className="banner-image"
+          />
+        </div>
+      </div>
+    </main>
+  );
+};
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,37 @@
+import countryData from "../api/countryData.json";
 
 export const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <section className="about-section container">
+      <h2 className="container-title">
+        Want to know about the Facts <br />
+        Peoples are proud of
+      </h2>
+
+      <div className="container-card">
+        {countryData.map((currentCountry) => {
+      const { id, countryName, capital, population, interestingFact } = currentCountry;
+          return (
+            <div className="card" key={id}>
+              <div className="container">
+                <h3 className="card-title">{countryName}</h3>
+                <p>
+                  <span className="card-description">Capital: </span>
+                  {capital}
+                </p>
+                <p>
+                  <span className="card-description">Population: </span>
+                  {population}
+                </p>
+                <p>
+                  <span className="card-description">Interesting Fact: </span>
+                 {interestingFact}
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};

ee196301f945c6b4c24470e9b176b1b444cdf3fc
Mujtaba Jawadi <mujtabafalcon986@gmail.com>
2024-10-20 10:30:06 +0500

    Done with Contact us page and handling Form Data with Reactv19

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,37 @@
-
 export const Contact = () => {
+
+  const handleFormSubmit = (formData)=>{
+    const formInputData = Object.fromEntries(formData.entries());
+    console.log(formInputData)
+
+  }
+
+
   return (
-    <div>Contact</div>
-  )
-}
+    <section className="contact-section">
+      <div className="container">
+      <h2 className="container-title">Contact Us</h2>
+        <form action={handleFormSubmit}>
+          <input
+            type="text"
+            name="username"
+            required
+            placeholder="Enter your name"
+            className="form-control"
+            autoComplete="off"
+          />
+          <input
+            type="email"
+            name="email"
+            required
+            autoComplete="off"
+            placeholder="Enter your Email"
+            className="form-control"
+          />
+          <textarea name="message" required rows="10" placeholder="Write to US..." autoComplete="off" className="form-control"></textarea>
+          <button type="submit" value="send" className="form-btn">Send</button>
+        </form>
+      </div>
+    </section>
+  );
+};
